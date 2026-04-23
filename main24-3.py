98b5b26c2cbd57398733c4c9b4c6bf3658f42ef5
J-HyunBeen <hyunbin309@naver.com>
2026-04-24 00:20:49 +0900

    main24-3.pygo

---
+++ main24-3.py
@@ -0,0 +1,28 @@
+import urllib.request, json, tkinter, tkinter.font
+ 
+API_KEY = "48fde1e95a9d04732d98226e3be018a7"
+
+ 
+def tick1Min():
+    url = f"https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid={API_KEY}&units=metric"
+    with urllib.request.urlopen(url) as r:
+        data = json.loads(r.read())
+        print(data)
+    temp = data["main"]["temp"]
+    humi = data["main"]["humidity"]
+    label.config(text=f"{temp:.1f}C   {humi}%")
+    window.after(60000, tick1Min)
+ 
+window = tkinter.Tk()
+window.title("TEMP HUMI DISPLAY")
+window.geometry("400x100")
+window.resizable(False, False)
+font = tkinter.font.Font(size=30)
+label = tkinter.Label(window, text="", font=font)
+label.pack()
+tick1Min()
+window.mainloop()
+
+
+
+
